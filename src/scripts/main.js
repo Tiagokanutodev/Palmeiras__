9eed66a38288b25ed385173bc0be7f5368e1b6b8
tiago canuto <tiagocanutogames@gmail.com>
2024-02-06 20:34:45 -0300

    atualizando, data e hora

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Mar 22, 2024 00:00:01");
+const dataDoEvento = new Date("Mar 22, 2024 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function()
